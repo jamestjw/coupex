a06cf1f7602d9aecb36c8694318829bb85fcd096
James Tan <james_tan97@outlook.com>
2026-04-25 11:59:02 -0400

    [skip ci] remove some obsolete comments

--- lib/coupex_web/router.ex
+++ lib/coupex_web/router.ex
@@ -28,21 +28,4 @@ defmodule CoupexWeb.Router do
   # scope "/api", CoupexWeb do
   #   pipe_through :api
   # end
-
-  # Enable LiveDashboard and Swoosh mailbox preview in development
-  if Application.compile_env(:coupex, :dev_routes) do
-    # If you want to use the LiveDashboard in production, you should put
-    # it behind authentication and allow only admins to access it.
-    # If your application does not have an admins-only section yet,
-    # you can use Plug.BasicAuth to set up some basic authentication
-    # as long as you are also using SSL (which you should anyway).
-    import Phoenix.LiveDashboard.Router
-
-    scope "/dev" do
-      pipe_through :browser
-
-      live_dashboard "/dashboard", metrics: CoupexWeb.Telemetry
-      forward "/mailbox", Plug.Swoosh.MailboxPreview
-    end
-  end
 end
