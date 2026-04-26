d4db5bdc5b4ef8206af612c63c0f5a22d364bee8
James Tan <james_tan97@outlook.com>
2026-04-26 12:20:35 -0400

    structure production logs for Loki

--- config/prod.exs
+++ config/prod.exs
@@ -29,7 +29,12 @@ config :swoosh, api_client: Swoosh.ApiClient.Req
 config :swoosh, local: false
 
 # Do not print debug messages in production
-config :logger, level: :info
+config :logger,
+  level: :info,
+  default_formatter: [
+    format: {CoupexWeb.LoggerFormatter, :format},
+    metadata: [:request_id]
+  ]
 
 # Runtime production configuration, including reading
 # of environment variables, is done on config/runtime.exs.
